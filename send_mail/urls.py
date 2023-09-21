1eec7fd234b1487ea004a9aa79f21a6775a52677
Alex Klepinin <133595809+klepininalex@users.noreply.github.com>
2023-09-21 11:36:49 +0500

    Update urls.py

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from send_mail.apps import SendMailConfig
 
-from views import *
+from send_mail.views import *
 
 app_name = SendMailConfig.name
 
 urlpatterns = [
-    path('', ClientListView.as_view(), name='index')
+    path('', MailListView.as_view(), name='index'),
+    path('mail/create/', MailCreateView.as_view(), name='create_mail'),
+    path('mail/view/<int:pk>', MailDetailView.as_view(), name='view_mail'),
+    path('mail/edit/<int:pk>', MailUpdateView.as_view(), name='edit_mail'),
+    path('mail/delete/<int:pk>', MailDeleteView.as_view(), name='delete_mail'),
 ]
