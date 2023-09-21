cf93c2e89b5a274f3e261c7c370459db5c2bc3f0
Alex Klepinin <133595809+klepininalex@users.noreply.github.com>
2023-09-21 11:36:20 +0500

    Update views.py

--- send_mail/views.py
+++ send_mail/views.py
@@ -1,7 +1,47 @@
-from django.views.generic import ListView
+from django.forms import inlineformset_factory
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from send_mail.models import Client
+from send_mail.forms import MailSettingsForm, ClientForm, TextMailForm
+from send_mail.models import MailSettings, TextMail, Client
 
 
-class ClientListView(ListView):
-    model = Client
+class MailListView(ListView):
+    model = MailSettings
+    template_name = 'send_mail/mail_list.html'
+
+
+class MailCreateView(CreateView):
+    model = MailSettings
+    form_class = MailSettingsForm
+    success_url = reverse_lazy('send_mail:index')
+    template_name = 'send_mail/mail_form.html'
+
+    # def form_valid(self, form):
+    #     response = super().form_valid(form)
+    #     send_mailing(self.object)
+    #     return response
+
+
+class MailDetailView(DetailView):
+    model = MailSettings
+    template_name = 'send_mail/mail_detail.html'
+
+
+class MailUpdateView(UpdateView):
+    model = MailSettings
+    form_class = MailSettingsForm
+    success_url = reverse_lazy('send_mail:index')
+    template_name = 'send_mail/mail_form.html'
+
+    # def form_valid(self, form):
+    #     response = super().form_valid(form)
+    #     send_mailing(self.object)
+    #     return response
+
+
+class MailDeleteView(DeleteView):
+    model = MailSettings
+    success_url = reverse_lazy('send_mail:index')
+    template_name = 'send_mail/mail_confirm_delete.html'
